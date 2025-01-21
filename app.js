a866b04ace792d6165cd996885abc5dcbf37a7bd
Rafakka <rafaelkaher@gmail.com>
2025-01-21 10:06:34 -0300

    tentativa de codigo

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaDeAmigos = [];
+
+function adicionarAmigo(){
+    let campoTexto = document.getElementById('amigo');
+    let amigoText = campoTexto.value.trim();
+
+    if (amigoText === ''){
+        alert('Por favor, insira um nome.');
+        return;
+    } else {
+        listaDeAmigos.push(amigoText);
+        campoTexto.value = '';
+        alert('Amigo adicionado: ' + amigoText);
+}
+    }
+
+function sortearAmigos (){
+    let listaSorteada = listaDeAmigos.slice();
+    let listaSorteadaEmbaralhada = [];
+
+    while (listaSorteada.length > 0){
+        let indiceSorteado = Math.floor(Math.random() * listaSorteada.length);
+        let amigoSorteado = listaSorteada.splice(indiceSorteado, 1);
+        listaSorteadaEmbaralhada.push(amigoSorteado);
+    }
+
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = listaSorteadaEmbaralhada.join(', ');
+}
+
